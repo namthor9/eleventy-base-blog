91b4f0bc2df55e14ef276b14d9aa0933cbfb8e57
namthor9 <62577935+namthor9@users.noreply.github.com>
2023-10-20 11:44:15 -0700

    first edits

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
-	url: "https://example.com/",
+	title: "butterfly powder",
+	url: "https://butterflypowder.netlify.com",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "nuerons firing make strange words which float in the infinite library",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "macks",
+		email: "maxrothman6@gmail.com",
+		url: "https://butterflypowder.netlify.com"
 	}
 }
